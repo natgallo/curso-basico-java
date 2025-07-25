b40979609f4d22226fe4d7707ba360c7828e0fbe
Natalia <natalia.galloribeiro09@gmail.com>
2025-07-25 07:24:47 -0300

    aula10variaveis

---
+++ eclipse/cursojavabasico/src/cursojava/aula10/Variaveis.java
@@ -0,0 +1,21 @@
+package cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		//Convenção Java
+		int idade = 20;
+		String nome = "Nat";
+		String nomeDoMeuCachorro = "Totó";
+		
+		idade = 25;
+		
+		System.out.println("Idade = "+ idade);
+		System.out.println("Nome = "+ nome);
+		System.out.println("Cachorro = "+ nomeDoMeuCachorro);
+		
+
+	}
+
+}

0a5ad48559d6495a9521546fa6a8bb128319387b
Natalia <natalia.galloribeiro09@gmail.com>
2025-07-31 12:50:20 -0300

    Exercício 15, Lista Dois

--- eclipse/cursojavabasico/src/cursojava/listaExerciciosDois/Exerc15.java
+++ eclipse/cursojavabasico/src/cursojava/listaExerciciosDois/Exerc15.java
@@ -1,9 +1,29 @@
 package cursojava.listaExerciciosDois;
 
+import java.util.Scanner;
+
 public class Exerc15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		int ladoUm = scan.nextInt();
+		int ladoDois = scan.nextInt();
+		int ladoTres = scan.nextInt();
+
+		if (((ladoUm + ladoDois) > ladoTres) && ((ladoDois + ladoTres) > ladoUm) && ((ladoTres + ladoUm) > ladoDois)) {
+			if (ladoUm == ladoDois && ladoDois == ladoTres) {
+				System.out.println("Equilátero");
+			} else if (ladoUm == ladoDois || ladoUm == ladoTres || ladoDois == ladoTres) {
+				System.out.println("Isósceles");
+			} else {
+				System.out.println("Escaleno");
+			}
+		} else {
+			System.out.println("Não é triangulo.");
+		}
+		
+		scan.close();
 
 	}
 

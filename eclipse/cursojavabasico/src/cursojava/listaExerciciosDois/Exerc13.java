605b1e9d1067910f9e28c11ef297983ca819f40b
Natalia <natalia.galloribeiro09@gmail.com>
2025-07-30 22:44:49 -0300

    Exercício 13 - Lista Dois

--- eclipse/cursojavabasico/src/cursojava/listaExerciciosDois/Exerc13.java
+++ eclipse/cursojavabasico/src/cursojava/listaExerciciosDois/Exerc13.java
@@ -1,10 +1,26 @@
 package cursojava.listaExerciciosDois;
 
+import java.util.Scanner;
+
 public class Exerc13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite um número para o dia da semana: (1-7)");
+		
+		int dia = scan.nextInt();
+		
+		switch(dia) {
+		case 1: System.out.println("Domingo"); break;
+		case 2: System.out.println("Segunda"); break;
+		case 3: System.out.println("Terça"); break;
+		case 4: System.out.println("Quarta"); break;
+		case 5: System.out.println("Quinta"); break;
+		case 6: System.out.println("Sexta"); break;
+		case 7: System.out.println("Sábado"); break;
+		default: System.out.println("Valor Inválido"); break;
+		}
 	}
 
 }
